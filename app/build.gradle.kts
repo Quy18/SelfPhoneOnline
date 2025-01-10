586dce7d169a5f709055bda4bc45c876bfd6395c
Quy18 <maiquy18092003@gmail.com>
2025-01-11 00:49:19 +0700

    Adapter LoaiSp

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-    implementation("com.github.bumptech.glide:glide:4.16.0")
-    annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
+    implementation("com.github.bumptech.glide:glide:4.15.1")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.15.1")
 }
--- app/src/main/java/com/example/selfphoneonline/activity/MainActivity.java
+++ app/src/main/java/com/example/selfphoneonline/activity/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.selfphoneonline;
+package com.example.selfphoneonline.activity;
 
 import android.os.Bundle;
 import android.view.animation.Animation;
@@ -14,6 +14,9 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.example.selfphoneonline.R;
+import com.example.selfphoneonline.adapter.LoaiSpAdapter;
+import com.example.selfphoneonline.module.LoaiSp;
 import com.google.android.material.navigation.NavigationView;
 
 import java.util.ArrayList;
@@ -26,24 +29,29 @@ public class MainActivity extends AppCompatActivity {
     NavigationView navigationView;
     ListView listViewManHinhChinh;
     DrawerLayout drawerLayout;
+    LoaiSpAdapter loaiSpAdapter;
+    List<LoaiSp> arrLoaiSp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        anhXa();
+        AnhXa();
         ActionBar();
         ActionViewFlipper();
     }
 
     private void ActionViewFlipper() {
         List<String> arrQuangCao = new ArrayList<>();
-        arrQuangCao.add("https://www.youtube.com/redirect?event=video_description&redir_token=QUFFLUhqbVVHVlQ3NVRPMkVKS1VFcFp6WTRRV1RhZ1lVZ3xBQ3Jtc0ttd0l2NXJsUWVCR3dOakgwdlRwSE5SdGhoQlB5V2sxclJuUHpGUjV2UV9iVUhnMHFiOFNJTjk3UHVTYVpHOUIxbGZEcUhndFUyRldtNGdMbHRUTXEzQWVIeEF3XzZIeDZtd1lPb1ByME1LMTlOWHVzQQ&q=http%3A%2F%2Fmauweb.monamedia.net%2Fthegioididong%2Fwp-content%2Fuploads%2F2017%2F12%2Fbanner-Le-hoi-phu-kien-800-300.png&v=QAqXA_bpD5o");
-        arrQuangCao.add("https://www.youtube.com/redirect?event=video_description&redir_token=QUFFLUhqa3FSRHAxSjVlVk1XdzZXYUlBNnhqN191VG40UXxBQ3Jtc0tsd2VUTXJpQThMdjhycDBJektjMWNyWVdfSnFUNXhJUHBVbHBNNEY1MlNvcnFBb1Bka3VVMjZST0ZPa3J0UkhZTkh2OG9PV3JYR005UWFtc1lIS01XRWdLYmkwakZQRko3ZmtPalJQRHY3VnNnWHlVcw&q=http%3A%2F%2Fmauweb.monamedia.net%2Fthegioididong%2Fwp-content%2Fuploads%2F2017%2F12%2Fbanner-HC-Tra-Gop-800-300.png&v=QAqXA_bpD5o");
-        arrQuangCao.add("https://www.youtube.com/redirect?event=video_description&redir_token=QUFFLUhqa210NEN6LTNaN0U4bk5MdnU4b01RdlhTQ2UtQXxBQ3Jtc0tuX2s5M0FoVHpZWEFueWdYbk5WZG9XQ090SkJ6WWVyaUhPQnh4N0hqcFVRWjRORDVwWnFqVGp2d1l4T0ttWVVyY2FSQ3A5VDktZ0h6cFdienJRTzI0MDBWbnE3SjdCUV9mME5JUzFvLXMwSU9KUG9Kbw&q=http%3A%2F%2Fmauweb.monamedia.net%2Fthegioididong%2Fwp-content%2Fuploads%2F2017%2F12%2Fbanner-big-ky-nguyen-800-300.jpg&v=QAqXA_bpD5o");
+        arrQuangCao.add("https://mauweb.monamedia.net/thegioididong/wp-content/uploads/2017/12/banner-Le-hoi-phu-kien-800-300.png");
+        arrQuangCao.add("https://mauweb.monamedia.net/thegioididong/wp-content/uploads/2017/12/banner-HC-Tra-Gop-800-300.png");
+        arrQuangCao.add("https://mauweb.monamedia.net/thegioididong/wp-content/uploads/2017/12/banner-big-ky-nguyen-800-300.jpg");
+
         for (int i = 0; i < arrQuangCao.size(); i++) {
             ImageView imageView = new ImageView(getApplicationContext());
-            Glide.with(getApplicationContext()).load(arrQuangCao.get(i)).into(imageView);
+            Glide.with(getApplicationContext())
+                    .load(arrQuangCao.get(i))
+                    .into(imageView);
             imageView.setScaleType(ImageView.ScaleType.FIT_XY);
             viewFlipper.addView(imageView);
         }
@@ -64,12 +72,19 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public void anhXa(){
+    public void AnhXa(){
         toolbar = findViewById(R.id.toolbarmanhinhchinh);
         viewFlipper = findViewById(R.id.viewflipper);
         recyclerViewManHinhChinh = findViewById(R.id.recycleview);
         navigationView = findViewById(R.id.navigationview);
         listViewManHinhChinh = findViewById(R.id.listviewmanhinhchinh);
         drawerLayout = findViewById(R.id.drawerLayout);
+
+        // Khởi tạo danh sách sản phẩm
+        arrLoaiSp = new ArrayList<>();
+
+        // Khởi tạo Adapter
+        loaiSpAdapter = new LoaiSpAdapter(getApplicationContext(), arrLoaiSp);
+        listViewManHinhChinh.setAdapter(loaiSpAdapter);
     }
 }
